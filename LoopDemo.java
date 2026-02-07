8114f4b97563e1bea30becb41db61c0dd388013c
Carlo Sandoval <carlossandoval812@gmail.com>
2026-02-07 20:25:03 +0800

    Create LoopDemo.java

---
+++ LoopDemo.java
@@ -0,0 +1,38 @@
+class LoopDemo{
+  public static void main (String cals[]){
+
+  //for loop
+    int i=1;
+      for (1<=10;++i)
+        System.out.print(i+" ");
+//while loop
+  int i=1;
+    while(1<=10) {
+    System.out.print(i+" ");
+    ++i;
+    }
+//do-while loop
+  int i=1;
+    do{
+     System.out.print(i+ " ");
+      ++i;
+    }while(i<=10);
+//nested loop
+    for (int x=1;x<=5;++x){
+      for int y=1;y<=5;++y)
+        System.out.print ("|"+(x*y)+"|\t");
+    }
+//inverted triangle
+    for (int a=5; a>=l; --a){ 
+      for (int b=1; b<=a; ++b)
+        System.out.print("*\t");
+    System.out.println():
+    }
+//right triangle
+    for (int a=1;a<=5;++a){
+      for (int b=1; b<=a; ++b)
+        System. out print("*\t"):
+    System.out.printIn() :
+    }
+  }
+}
