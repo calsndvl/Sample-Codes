9ed15b15f48ce89364dfa3090e255e40a5536a08
Carlo Sandoval <carlossandoval812@gmail.com>
2026-02-07 21:21:58 +0800

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Switch{
+  public static void main(String cals[]){
+  Scanner s = new Scanner(System.in);
+
+  System.out.print("Enter day of the week (1-7): ");
+  int day = s.nextInt();
+
+    switch(day){
+      case 1: System.out.println("Monday"); break;
+      case 2: System.out.println("Tuesday"); break;
+      case 3: System.out.println("Wednesday"); break;
+      case 4: System.out.println("Thursday"); break;
+      case 5: System.out.println("Friday"); break;
+      case 6: System.out.println("Saturday"); break;
+      case 7: System.out.println("Sunday"); break;
+      default: System.out.println("Out of bounds!");
+    
+    }
+  }
