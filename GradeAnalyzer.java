4e0a31052da9ee50115a774e385b7cda23d76e8e
Carlo Sandoval <carlossandoval812@gmail.com>
2026-02-09 20:39:05 +0800

    Update GradeAnalyzer.java

--- GradeAnalyzer.java
+++ GradeAnalyzer.java
@@ -1,26 +1,30 @@
 import java.util.*;
+class GradeAnalyzer{
+  public static void main(String cals[]){
+  Scanner s = new Scanner(System.in);
+  char choice;
+  /*
+    SUM = N1+N2;
+    DIFFERENCE = N1-N2;
+    PRODUCT = N1*N2;
+    QUOTIENT = N1/N2;
+    REMAINDER = N1%N2;
+*/
+do{
+    System.out.print("Enter grade: ");
+    int grade=s.nextInt();
 
-public class GradeAnalyzer{
-	public static void main(String cals[]){
-	Scanner scanner = new Scanner(System.in);
-	char choice;
-	do{
-		System.out.print("Enter grade: ");
-		int grade = scanner.nextInt();
-
-		if(grade >= 95)
-			System.out.println("Excellent!");
-		else if(grade >= 90)
-			System.out.println("Very Good!");
-		else if(grade >= 75)
-			System.out.println("Good!");
-		else
-			System.out.print("Failed!");
+    if(grade>=90)
+      System.out.print("Excellent!");
+    else if (grade>=80)
+      System.out.print("Very Good!");
+    else if (grade>=75)
+      System.out.print("Passed!");
+    else
+      System.out.print("Failed, better luck next time.");
+    
 		System.out.print("Do you want to analyze another grade(Y/N)? ");
 		choice = scanner.next().charAt(0);
-
-
 		}while(choice == 'Y');
-
 	}
 }
